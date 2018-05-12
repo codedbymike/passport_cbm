d0781a58e040bb70ece7afab99e5d66f115f7812
codedbymike <codedbymike@gmail.com>
2018-05-12 10:07:41 -0700

    Folder creation for passport, services, and routes

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
 const express = require("express");
+
+require("./services/passport");
 const app = express();
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys.js");
+
 //app.use(express.static("public"));
 //app.set("view engine", "ejs");
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    accessToken => {
-      console.log(accessToken);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("server has started, visit " + PORT));
