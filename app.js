b2a7ae650fef6431faaacebefeeb9ba9447df4e1
codedbymike <codedbymike@gmail.com>
2018-05-11 18:22:23 -0700

    added in passport and google oauth

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const app = express();
-//import express from "express";
-//const request = require("request");
-//const port = 3000
-
-app.use(express.static("public"));
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys.js");
+//app.use(express.static("public"));
 //app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("server has started, visit " + PORT));
